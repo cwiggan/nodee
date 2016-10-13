e1119e9fc94143d98ea861cf0a135611617b21b7
cwiggan <info@cwiggan.com>
2016-10-13 15:56:03 -0400

    update function to es6

--- app/routes.js
+++ app/routes.js
@@ -5,7 +5,7 @@ var path = require('path');
 
 var router = express.Router();
 
-router.get('/', function(req, res){
+router.get('/', (req, res) => {
 	res.render('index');
 });
 
@@ -13,7 +13,7 @@ router.get('/', function(req, res){
 module.exports = router;
 
 //about page
-router.get('/about', function(req, res){
+router.get('/about', (req, res) => {
 	
 	var users = [
 		{ name:'Molly', email:'me@mail.com', site:'molly.com', img: 'https://unsplash.it/200/300/?random' },
@@ -25,10 +25,10 @@ router.get('/about', function(req, res){
 
 });
 
-router.get('/contact', function(req, res){
+router.get('/contact', (req, res) => {
 	res.render('contact');
 });
-router.post('/form', function(req, res){
+router.post('/form', (req, res) => {
 	res.send('Thank For Contacting Us ' + req.body.name + '! We will get back to you soon);
 });
 
